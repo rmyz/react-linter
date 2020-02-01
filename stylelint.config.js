0c8c30b008180efc44e28c1d4dfee2ee2bd0ea48
Sergi Romeu <romeuhd@gmail.com>
2020-02-01 21:12:46 +0100

    update stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,4 +2,7 @@ module.exports = {
   processors: ['stylelint-processor-styled-components'],
   plugins: ['stylelint-prettier'],
   extends: ['stylelint-config-recommended', 'stylelint-config-styled-components', 'stylelint-config-prettier'],
+  rules: {
+    'property-no-vendor-prefix': null,
+  },
 };
